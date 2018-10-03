ef471e112736f64fb156fff5401ee761083872cb
Ihsaan Patel <ihsaan.patel@gmail.com>
2018-10-03 00:09:42 -0400

    fix deployment package

--- recommendation_generator/create_lambda_deployment_package.sh
+++ recommendation_generator/create_lambda_deployment_package.sh
@@ -2,4 +2,4 @@ cd recommendation-env/lib/python3.6/site-packages/
 zip -r9 ../../../../RecsLambdaDeploymentPackage.zip *
 cd ../../../../
 zip -g RecsLambdaDeploymentPackage.zip generate_recommendations.py
-zip -g RecsLambdaDeploymentPackage.zip utils
+zip -r -g RecsLambdaDeploymentPackage.zip utils
