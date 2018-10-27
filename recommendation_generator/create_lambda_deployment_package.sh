28f8037d9db64c8568d4be551126a89c508140bc
Ihsaan Patel <ihsaan.patel@gmail.com>
2018-10-26 22:22:45 -0400

    update deploy script

--- recommendation_generator/create_lambda_deployment_package.sh
+++ recommendation_generator/create_lambda_deployment_package.sh
@@ -1,3 +1,4 @@
+pip freeze > requirements.txt
 docker build --rm --no-cache -t lambda-recommendations .
 
 container_id=$(docker create lambda-recommendations bash)
@@ -5,3 +6,4 @@ docker cp $container_id:/var/task/RecsLambdaDeploymentPackage.zip .
 docker rm -v $container_id
 
 aws s3 cp RecsLambdaDeploymentPackage.zip s3://elasticbeanstalk-us-east-1-811388761146/lambda_function_code/
+aws lambda update-function-code --function-name weeklyRecommedationGenerator --s3-bucket elasticbeanstalk-us-east-1-811388761146 --s3-key lambda_function_code/RecsLambdaDeploymentPackage.zip
